08fdaca5728ffffc3c6a4a91e689aa2d832d017c
cydia2020 <12470297+cydia2020@users.noreply.github.com>
2022-08-30 13:13:15 +1000

    gracefully cancel ACC if lead starts moving and the brakes are still held

--- selfdrive/car/toyota/carcontroller.py
+++ selfdrive/car/toyota/carcontroller.py
@@ -133,7 +133,7 @@ class CarController:
 
     # we can spam can to cancel the system even if we are using lat only control
     if (self.frame % 3 == 0 and self.CP.openpilotLongitudinalControl) or pcm_cancel_cmd:
-      lead = hud_control.leadVisible or (CS.out.vEgo < 12. and (not CS.out.standstill or CC.enabled))  # at low speed we always assume the lead is present so ACC can be engaged
+      lead = hud_control.leadVisible or (CS.out.vEgo < 12. and (not CS.out.standstill or CC.enabled)) or (not (CS.out.standstill and CS.out.brakePressed and not lead_vehicle_stopped )) # at low speed we always assume the lead is present so ACC can be engaged
 
       # Lexus IS uses a different cancellation message
       if pcm_cancel_cmd and self.CP.carFingerprint in (CAR.LEXUS_IS, CAR.LEXUS_RC):
