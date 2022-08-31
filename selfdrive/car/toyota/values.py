96e222cc4a885953b773abb87f0232b08254500c
jpM1-462 <112355481+jpM1-462@users.noreply.github.com>
2022-08-31 12:19:22 +0900

    Update values.py
    アクセル強度

--- selfdrive/car/toyota/values.py
+++ selfdrive/car/toyota/values.py
@@ -14,7 +14,7 @@ PEDAL_TRANSITION = 10. * CV.MPH_TO_MS
 
 
 class CarControllerParams:
-  ACCEL_MAX = 1.5  # m/s2, lower than allowed 2.0 m/s2 for tuning reasons
+  ACCEL_MAX = 0.8  # m/s2, lower than allowed 2.0 m/s2 for tuning reasons
   ACCEL_MIN = -4.0  # m/s2
 
   STEER_MAX = 1500
